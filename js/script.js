9ec3b4fdbee7a1ec050c3b35fed891fe64094ed7
rizaldika123 <rizaldika03@gmail.com>
2024-05-03 06:05:51 +0700

    membuat website karockopi-coffee-shop

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//Togle class active
+const navbarNav = document.querySelector(".navbar-nav");
+//Ketika Humberger Menu DiKlik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//Klik Di Luar Sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
